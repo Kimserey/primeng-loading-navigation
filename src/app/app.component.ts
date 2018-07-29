c4ecc6b6bd355e806b3aaf12b4e100e5781701d8
Kimserey <kimserey.lam@groomgy.com>
2018-07-29 19:59:49 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    Hello world
+    <app-choice></app-choice>
   `
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { ButtonModule } from 'primeng/button';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 
 import { AppComponent } from './app.component';
+import { ChoiceComponent } from './choice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChoiceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    OverlayPanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
